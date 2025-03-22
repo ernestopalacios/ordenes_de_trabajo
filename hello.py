e65660b10b5a342cbb0c0fd962ad6957ce8ab783
Ernesto Palacios <vlad@xps-9510.com>
2025-03-22 10:51:07 -0500

    Funciona, falta limpiar bien al final de ejecución, pero lo ignoraremos por ahora

--- hello.py
+++ hello.py
@@ -1,28 +1,39 @@
 import sys
 import os.path
-
 import logging
+import queue
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler, FileSystemEventHandler, FileCreatedEvent, FileModifiedEvent
+from watchdog.events import (
+    FileSystemEventHandler,
+    FileCreatedEvent,
+    FileModifiedEvent,
+)
+import time
+from datetime import datetime
 
+from eerssa import gestionOT
+from eerssa import matrizActividades
+from pathlib import Path
+import pandas as pd
 
-
+from dask.distributed import Client, LocalCluster, get_client
 
 class MyEventHandler(FileSystemEventHandler):
     """
-    Custom event handler that appends created and modified files to a list.
+    Custom event handler that appends created and modified files to a queue.
     """
 
-    def __init__(self):
+    def __init__(self, client):
         super().__init__()
-        self.file_list = []
+        self.file_queue = queue.Queue()
+        self.client = client # DASK LocalClient
 
     def on_created(self, event):
         """
         Called when a file or directory is created.
         """
         if isinstance(event, FileCreatedEvent):
-            self.file_list.append(event.src_path)
+            self.file_queue.put(event.src_path)
             logging.info(f"File created: {event.src_path}")
 
     def on_modified(self, event):
@@ -30,67 +41,134 @@ class MyEventHandler(FileSystemEventHandler):
         Called when a file or directory is modified.
         """
         if isinstance(event, FileModifiedEvent):
-            self.file_list.append(event.src_path)
+            self.file_queue.put(event.src_path)
             logging.info(f"File modified: {event.src_path}")
 
-    def get_file_list(self):
+    def get_queue(self):
         """
-        Returns the list of created and modified files.
+        Returns the queue of created and modified files.
         """
-        return self.file_list
+        return self.file_queue
+
+    def process_all_items(self):
+        """
+        Removes and processes all items from the queue at once.
+        """
+        items_to_process = []
+        while not self.file_queue.empty():
+            try:
+                item = self.file_queue.get_nowait()
+                items_to_process.append(item)
+                self.file_queue.task_done()
+            except queue.Empty:
+                break
+
+        if len(items_to_process) > 8:
+            start_time = time.time()
+            start_datetime = datetime.now()
+            print(
+                f"   Procesando {len(items_to_process)} archivos. Hora de inicio: {start_datetime.strftime('%Y-%m-%d %H:%M:%S')}"
+            )
+            items_to_process = list(set(items_to_process))
+
+            futures = [
+                self.client.submit(gestionOT.GestionOt, file, actor=True)
+                for file in items_to_process
+            ]
+            ot_array = [future.result() for future in futures]
+            ot_cargada = [ot.load_ot() for ot in ot_array]
+            obj_lists = [future.result() for future in ot_cargada]
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(
+                f"   Procesados todos los {len(obj_lists)} items. Tiempo transcurrido: {elapsed_time:.2f} segundos.\n"
+            )
+
+        elif len(items_to_process) > 0:
+            obj_lists = []
+            obj_data = []
+            for file in items_to_process:
+                ot = gestionOT.GestionOt(file)
+                ot.load_ot()
+                obj_lists.append(ot)
+                obj_data.append(ot.data)
+
+            print(f"   Procesados todos los {len(obj_lists)} items.\n")
 
 
-def main():
+def main(event_handler):
     """
     Monitors a directory for file creation and modification events.
     """
-    return
+    while True:
+        event_handler.process_all_items()
+        time.sleep(0.5)
+
+
+def get_or_create_client():
+    """
+    Checks if a Dask client is already running. If so, connects to it.
+    Otherwise, creates a new LocalCluster and client.
+    """
+    try:
+        # Try to get the default client (if one exists)
+        client = get_client()
+        print("   Conectado a un cluster existente.")
+        return client
+    except ValueError:
+        # No client exists, create a new LocalCluster and client
+        print("   Creando un nuevo cluster local.")
+        cluster = LocalCluster()
+        client = Client(cluster)
+        return client
+
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
-    # Configure logging
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    
+    # Get or create the Dask client
+    client = get_or_create_client()
+
     # Object creation
-    event_handler = MyEventHandler()
+    event_handler = MyEventHandler(client)
     observer = Observer()
 
-    
     print("\n   === Monitor de Ordenes de trabajo ====")
 
-
-
     if len(sys.argv) > 1:
-        # if there is an argument 
-        base_dir = sys.argv[1]       
+        base_dir = sys.argv[1]
     else:
-        print(  "   Es necesario introducir el directorio (carpeta) donde se almacenarán las órdenes de trabajo")
+        print(
+            "   Es necesario introducir el directorio (carpeta) donde se almacenarán las órdenes de trabajo"
+        )
         base_dir = input("   Por favor, introduce el directorio: ")
 
-    #Validate the directory
-    if os.path.isdir(base_dir):
-        print(f"   Se ha iniciado a monitorear el directorio: \n   ==>: '{base_dir}'\n")
+    if not os.path.isdir(base_dir):
+        print(f"   Error: > '{base_dir}' < no es un directorio válido.\n\n")
+    else:
+        print(
+            f"   Se ha iniciado a monitorear el directorio:\n   ==>: '{base_dir}'\n"
+        )
 
-        observer.schedule(event_handler, base_dir, recursive=False)  # recursive False, only monitor base directory
+        observer.schedule(event_handler, base_dir, recursive=False)
         observer.start()
+
         try:
+            main(event_handler)
             while observer.is_alive():
                 observer.join(1)
         except KeyboardInterrupt:
             print("\n   === Monitor de Ordenes de trabajo detenido ====\n\n")
+
         finally:
             observer.stop()
             observer.join()
-            
-            # Print the list of files after the observer has stopped
-            print("\n   === Lista de archivos creados o modificados ===")
-            for file_path in event_handler.get_file_list():
-                print(f"   - {file_path}")
-            print("\n")
-    else:
-            print(f"   Error: > '{base_dir}' < no es un directorio válido.\n\n")
+            client.close()
+            client.cluster.close()
 
-    
-    main()
+            print("\n   === Fin del proceso ===\n")
